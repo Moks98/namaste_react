9e2e77d815c86fe186bd09401b1cebde241a6a97
moks98 <mokshadiva98@gmail.com>
2023-07-31 00:01:45 +0530

    episode 1 - Inception

---
+++ App.js
@@ -0,0 +1,19 @@
+const heading = React.createElement(
+    
+    "div",{id:"parent1"},[
+        React.createElement("div",{id:"child1"},[ 
+            React.createElement("h1",{},"I am h1"),
+            React.createElement("h2",{},"I am h2"),
+        ]),
+     
+    
+            React.createElement("div",{id:"child2"},[ 
+                React.createElement("h1",{},"I am h1 child2"),
+                React.createElement("h2",{},"I am h2 child2"),
+            ]),
+        ]);
+
+
+
+ const root = ReactDOM.createRoot(document.getElementById("root"));
+ root.render(heading);//object
